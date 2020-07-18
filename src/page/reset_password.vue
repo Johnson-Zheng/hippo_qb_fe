9aca407968f413c3073d81fba6bda220c10a1778
gxj <940331234@qq.com>
2020-07-18 12:01:20 +0800

    添加注释、更新使用方法

--- src/page/reset_password.vue
+++ src/page/reset_password.vue
@@ -1,6 +1,7 @@
 <template>
 <div><h1>Register</h1>
-<CourseQuery @courseid="showcid"></CourseQuery>
+
+<CourseQuery @courseid="showcid"></CourseQuery>//复制这里 引用组件
     <a>查询结果：{{qdata}}</a>
 </div>
 </template>
@@ -12,10 +13,11 @@
         components:{CourseQuery},
         data(){
             return{
-                qdata:''
+                qdata:''//接受组件值
             }
         },
         methods:{
+            //接受组件传递参数的方法（复制粘贴把this.内容换成自己需要赋值的参数即可）
             showcid:function (msg){
                 this.qdata=msg
             }
