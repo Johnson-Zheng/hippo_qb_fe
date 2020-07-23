c4de8aec3279c652a56db35702b29c2974954bb3
gxj <940331234@qq.com>
2020-07-23 15:41:35 +0800

    axios请求地址修改，适配服务器ngix更改

--- src/Global.js
+++ src/Global.js
@@ -1,5 +1,5 @@
-const devBaseUrl = 'http://localhost:8081';
-const proBaseUrl = 'api.hippo-exam.q7w.cn:8081';
+const devBaseUrl = 'http://api.hippo-exam.q7w.cn:8081';
+const proBaseUrl = '/api';
 let BASE_URL;
 process.env.NODE_ENV === 'development' ? BASE_URL = devBaseUrl : BASE_URL = proBaseUrl ;
 export default{
