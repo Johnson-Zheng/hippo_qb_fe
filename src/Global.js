ce07b88aca9d9864936cb33c27bc41db88a75692
Johnson-Zheng <442043039@qq.com>
2020-07-23 13:02:41 +0800

    20200723 请求地址判断

--- src/Global.js
+++ src/Global.js
@@ -1,4 +1,7 @@
-const BASE_URL = 'http://localhost:8081' ;
+const devBaseUrl = 'http://localhost:8081';
+const proBaseUrl = 'api.hippo-exam.q7w.cn:8081';
+let BASE_URL;
+process.env.NODE_ENV === 'development' ? BASE_URL = devBaseUrl : BASE_URL = proBaseUrl ;
 export default{
-BASE_URL
+    BASE_URL
 }
